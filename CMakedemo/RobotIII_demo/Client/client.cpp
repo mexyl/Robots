142af983a22515e757ed491cc6e6dd839ccce8bf
py0330 <py0330@sjtu.edu.cn>
2016-03-30 13:13:00 +0800

    sync

--- CMakedemo/RobotIII_demo/Client/client.cpp
+++ CMakedemo/RobotIII_demo/Client/client.cpp
@@ -23,7 +23,7 @@ int sendRequest(int argc, char *argv[], const char *xmlFileName)
 		cmdName = cmdName.substr(0, cmdName.rfind('.'));
 	}
 
-	/*添加命令的所有参数*/
+	// 添加命令的所有参数 //
 	for (int i = 1; i < argc; ++i)
 	{
 		cmdName = cmdName + " " + argv[i];
@@ -31,20 +31,22 @@ int sendRequest(int argc, char *argv[], const char *xmlFileName)
 
 
 
-	/*构造msg，这里需要先copy命令名称，然后依次copy各个参数*/
+	// 构造msg，这里需要先copy命令名称，然后依次copy各个参数 //
 	aris::core::Msg msg;
 	msg.copy(cmdName.c_str());
 
 
 
-	/*连接并发送msg*/
+	// 连接并发送msg //
 	aris::core::XmlDocument doc;
 
-	if (doc.LoadFile(xmlFileName) != 0)
-		throw std::logic_error("failed to read configuration xml file");
+	if (doc.LoadFile(xmlFileName) != 0)	throw std::logic_error("failed to read configuration xml file");
 
 	std::string ip = doc.RootElement()->FirstChildElement("Server")->Attribute("ip");
 	std::string port = doc.RootElement()->FirstChildElement("Server")->Attribute("port");
+
+	std::cout << "ip:" << std::endl;
+	std::cout << ip << std::endl;
 
 	aris::core::Socket conn;
 
